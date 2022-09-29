45c9d1a6aec7103f9ea136faedef3fa0eef4c815
HarisMalisevic <haris.m2002.hm@gmail.com>
2022-09-30 00:51:57 +0200

    html to php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="style.css" rel="stylesheet">
+        
+    </head>
+
+<body>
+    
+    <div>        
+    </div>
+
+</body>
+
+</html>
